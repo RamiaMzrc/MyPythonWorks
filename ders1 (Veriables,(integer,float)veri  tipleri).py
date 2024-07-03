42b73853c36ec069b0cf94530cb24dac501b185a
RamiaMzrc <167282459+RamiaMzrc@users.noreply.github.com>
2024-07-03 15:02:44 +0300

    Add files via upload

---
+++ ders1 (Veriables,(integer,float)veri  tipleri).py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+3*3
+
+
+# In[2]:
+
+
+3**3
+
+
+# In[3]:
+
+
+10%2
+
+
+# In[4]:
+
+
+11%2
+
+
+# In[5]:
+
+
+x=5
+
+
+# In[6]:
+
+
+type(x)
+
+
+# In[7]:
+
+
+y=2.5
+
+
+# In[8]:
+
+
+type(y)
+
+
+# In[9]:
+
+
+r= input("r: ")
+
+
+# In[11]:
+
+
+pi=3.14
+
+
+# In[12]:
+
+
+type(pi)
+
+
+# In[13]:
+
+
+r
+
+
+# In[14]:
+
+
+type("r")
+
+
+# In[16]:
+
+
+r_int=int(r)
+
+
+# In[17]:
+
+
+r_int
+
+
+# In[18]:
+
+
+type(r_int)
+
+
+# In[19]:
+
+
+r_int*pi*2
+
+
+# bu yazdığımız şeyler bize alanı verir
+
+# In[ ]:
+
+
+
+
