b984a913b06f4f1759e3f70309e46b7502b0a8ab
RamiaMzrc <167282459+RamiaMzrc@users.noreply.github.com>
2024-07-03 15:05:34 +0300

    Add files via upload

---
+++ ders2 ( striglere giriş ve ascape characters ).py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x="hello world"
+
+
+# In[2]:
+
+
+type(x)
+
+
+# yukarıda çıkan çıktı string dir yani dizgi anlamına gelir
+
+# In[3]:
+
+
+len(x)
+
+
+# len kavramı legth demektir bu ifande () içeriside istenen değişkenin uzunluğunu verir ("hello world") kavramı 11 değişkene sahiptir boşuta değişken sayılır
+
+# In[4]:
+
+
+print(x)
+
+
+# Ascape characters ( kaçış karakterleri)
+
+# In[6]:
+
+
+print("hello python")
+
+
+# In[7]:
+
+
+print("hello \npython")
+
+
+# In[9]:
+
+
+print("hello \n python")
+
+
+# In[10]:
+
+
+print("hello \tpython")
+
+
+# In[ ]:
+
+
+
+
